d2a5b3ae37a898a63699c32c1c68e5c615ae940f
Javi Medac <jvv0014@alu.medac.es>
2025-05-05 08:51:49 +0200

    cambios

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Prueba 1 ejemplo', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('failed test', async ({ page }) => {
+test('Prueba 2 ejemplo fallo', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
+  // await page.pause()
+
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get starttttttted' }).click();
+  const textoEsperado = await page.getByRole('link', { name: 'Docs' }).textContent()
+
+  expect(textoEsperado).toBe('Ejemplo para el fallo')
 
 });
 
-test('get started link', async ({ page }) => {
+test('Prueba 3 ejemplo fallo', async ({ page }, TestInfo) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -23,4 +27,5 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+	await TestInfo.attach('Prueba', { body: await page.screenshot(), contentType: 'image/png' });
 });
